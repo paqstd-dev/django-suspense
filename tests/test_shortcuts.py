a27ea388970066ce70f9b85f035c4a7fd113e4ff
Jules Robichaud-Gagnon <j.robichaudg@gmail.com>
2024-11-29 12:13:51 -0500

    fix compatibility with python 3.9

--- tests/test_shortcuts.py
+++ tests/test_shortcuts.py
@@ -9,6 +9,6 @@ async def test_render():
     request = HttpRequest()
     response = render(request, "test.html")
     streaming_content = response.streaming_content
-    assert (await anext(streaming_content)) == b'foo\n'
+    assert (await streaming_content.__anext__()) == b'foo\n'
     with pytest.raises(StopAsyncIteration):
-        await anext(streaming_content)
+        await streaming_content.__anext__()
