11fe5ffd2adb6e404da428cadd179214cc1b1ab1
Jules Robichaud-Gagnon <j.robichaudg@gmail.com>
2024-11-30 23:27:37 -0500

    fix missing / in class-view/

--- example/posts/urls.py
+++ example/posts/urls.py
@@ -4,6 +4,6 @@ from .views import PostTemplateView, post, posts
 
 urlpatterns = [
     path('', posts),
-    path('class-view', PostTemplateView.as_view()),
+    path('class-view/', PostTemplateView.as_view()),
     path('<str:slug>/', post),
 ]
